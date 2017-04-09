f29502008c700b8933095606125c7d2b1cbea43a
Ben Meier <benmeier@fastmail.com>
2017-04-09 14:14:23 +0200

    tweaking helper functions

--- example/main.go
+++ example/main.go
@@ -9,6 +9,11 @@ import (
 func main() {
 	fmt.Printf("Is stdout a tty? %t\n", terminfo.IsStdoutTerminal())
 	fmt.Printf("Is stderr a tty? %t\n", terminfo.IsStderrTerminal())
-	w, h, _ := terminfo.GetStderrDimensions()
+
+	w, h, _ := terminfo.GetStdoutDimensions()
+	w, h = terminfo.DimensionsOrDefault(w, h, 80, 30)
 	fmt.Printf("Dimensions of stdout: %d x %d\n", w, h)
+
+	w, h = terminfo.DimensionsOrMin(w, h, 80, 60)
+	fmt.Printf("Rounded dimensions: %d x %d\n", w, h)
 }
--- terminfo.go
+++ terminfo.go
@@ -32,3 +32,29 @@ func GetStdoutDimensions() (width, height int, err error) {
 func GetStderrDimensions() (width, height int, err error) {
 	return GetFileDimensions(os.Stderr)
 }
+
+// DimensionsOrDefault is a helper method to use default dimensions rather than the output dimensions
+// in cases where there was an error reading the dimensions.
+func DimensionsOrDefault(twidth, theight, defaultwidth, defaultheight int) (width, height int) {
+	width, height = twidth, theight
+	if width <= 0 {
+		width = defaultwidth
+	}
+	if height <= 0 {
+		height = defaultheight
+	}
+	return
+}
+
+// DimensionsOrMin is a helper method to use minimum dimensions rather than the output dimensions
+// in cases where there was an error or the dimensions were smaller than expected.
+func DimensionsOrMin(twidth, theight, minwidth, minheight int) (width, height int) {
+	width, height = twidth, theight
+	if width < minwidth {
+		width = minwidth
+	}
+	if height < minheight {
+		height = minheight
+	}
+	return
+}
